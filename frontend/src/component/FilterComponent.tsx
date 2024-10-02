32b68c783a223d9c00e5d7a4823ec28a14969222
Ameer jafar <ameerjafar123@gmail.com>
2024-10-02 11:10:03 +0530

    added the code

--- frontend/src/component/FilterComponent.tsx
+++ frontend/src/component/FilterComponent.tsx
@@ -1,19 +1,23 @@
 import { useState } from "react"
 import axios from "axios";
 const FilterComponent = () => {
-    const [ genreParams, setGenereParams ] = useState([[]]);
-    const [ language, setLanguage ] = useState([[]]);
+    const [ genreParams, setGenereParams ] = useState<string[]>([]);
+    const [ language, setLanguage ] = useState<string[]>([]);
     const valueLanguage = ['Tamil', 'English', 'Malayalam', 'Hindi', 'Telugu'];
     const valueGenre = ['Drama', 'Comedy', 'Thriller', 'Action', 'Sci-fi'];
     const [ movies, setMovies ] = useState([[]]);
     const filter = async () => {
         const response = await axios.get('http://localhost:3000/allmovies');
         const allMovies = response.data.message;
-        console.log(allMovies)
-        const filteredData = allMovies.filter((val: any) => {
-            return language.includes(val.language) || genreParams.includes(val.genre);
+        const filteredData = await allMovies.filter((val: any) => {
+            const lan = val.language.split(",");
+            const gen = val.genre.split(",");
+            console.log(val.language);
+            console.log(lan)
+            const ans1 = language.some(item => lan.includes(item));
+            const ans2 = genreParams.some(item => gen.includes(item));
+            return ans1 || ans2;
         })
-        console.log(filteredData);
         if(!filteredData) {
             setMovies(allMovies);
         }
@@ -23,14 +27,11 @@ const FilterComponent = () => {
     }
     const formHandler = (e: any) => {
         const { value, checked } = e.target;
-        if(['Tamil', 'English', 'Hindi', 'Malayalam', 'Telugu'].includes(value)) {
-            setLanguage(prev => [...prev, value]);
-            console.log(language);
+        if(value.includes(valueLanguage)) {
+            setLanguage(prev => [...prev, value])
         } else {
             setGenereParams(prev => [...prev, value]);
-            console.log(genreParams)
         }
-        console.log(value, checked);
         filter();
     }
     return (
@@ -52,8 +53,8 @@ const FilterComponent = () => {
                     })}
                 </form>
                 <button onClick = { () => {
-                    setLanguage([[]]);
-                    setGenereParams([[]]);
+                    setLanguage([]);
+                    setGenereParams([]);
                 }}>clear</button>
             </div>
             <div>{ movies.map((val: any) => {return <div>{val.genre}</div>}) } </div>
